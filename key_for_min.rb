9c4a29d8bbc625cb3b0c20d9674bb164764b62b9
Daisuke Hashimoto <daisukehashimoto39@gmail.com>
2020-04-23 23:31:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,8 @@ def key_for_min_value(name_hash)
   min_value = 0
   
   name_hash.each do |key, value|
-    if name_hash != nil
-      if min_value == 0
-        min_key = key
-        min_value = value
-      elsif value < min_value
-        min_key = key
-        min_value = value
-      end
+    if min_value == 0 || value < min_value
+      min_key, min_value = key, value
     end
   end
   
